1ae72bbda550dbe424bf1b7334bc71a7fb0f8ce0
procode3 <wr.t8r1@gmail.com>
2023-07-18 17:23:34 -0700

    Fixing bug on large odd numbers

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,25 +3,10 @@
 
 
 def minOperations(n):
-    """
-    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
-
-    Args:
-        n (int): The desired number of H characters.
-
-    Returns:
-        int: The minimum number of operations required. Returns 0 if n is impossible to achieve.
-    """
-
-    if n == 1:
+    """minimum operations"""
+    if n <= 1:
         return 0
-
-    dp = [0] * (n + 1)
-    for i in range(2, n + 1):
-        dp[i] = float('inf')
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n] if dp[n] != float('inf') else 0
-
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
+    return n
