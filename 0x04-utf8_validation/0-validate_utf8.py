f51d3aabf1b51570edfbde4caae23c3a143f00f0
procode3 <wr.t8r1@gmail.com>
2023-07-27 12:23:22 -0700

    Utf-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Utf8 Validation"""
+
+def validUTF8(data):
+        """
+        :type data: List[int]
+        :rtype: bool
+        """
+        count = 0
+        for num in data:
+            if count == 0 :                
+                if num >> 5 == 0b110:
+                   count = 1
+                elif num >> 4  == 0b1110:
+                   count = 2
+                elif num >> 3  == 0b11110:
+                   count = 3
+                elif not num >> 7 == 0b0:
+                    return False
+               
+            else:
+                if not num >> 6 == 0b10:
+                    return False
+                else:
+                    count -= 1
+        
+        return count == 0
